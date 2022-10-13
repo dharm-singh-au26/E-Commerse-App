653c175bdcc2c8c79ea6793d210337d087775dfc
Dharm Singh <dharmjat45@gmail.com>
2022-10-13 22:13:58 +0530

    DATABASE CONNECTION CREATED

---
+++ back-end/src/index.server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const env = require("dotenv");
+const mongoose = require("mongoose");
+const app = express();
+const bodyParser = require("body-parser");
+
+// environment variable
+env.config();
+
+// mongodb connection
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.2dlwwff.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Database Connected");
+  });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "hello from server",
+  });
+});
+app.post("/data", (req, res) => {
+  res.status(201).json({
+    message: req.body,
+  });
+});
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+});
