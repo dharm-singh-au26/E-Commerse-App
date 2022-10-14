43a7a1c849d1671289c58deb10b7d39de2fbd363
Dharm Singh <dharmjat45@gmail.com>
2022-10-14 23:19:01 +0530

    created routes

---
+++ back-end/src/routes/user.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/signin',(req,res) =>{
+
+})
+
+router.post('/signup',(req,res) =>{
+    
+})
+
+
+
+
+
+module.exports = router;
